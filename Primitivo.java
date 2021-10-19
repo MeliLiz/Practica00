f5e8ca083f87f1ba5ed91820411fb3370f30ba69
MeliLiz <melifernandez@ciencias.unam.mx>
2021-10-19 15:18:02 -0500

    tipos primitivos

---
+++ Primitivo.java
@@ -0,0 +1,14 @@
+public class Primitivo {
+        public static void main (String[] args){
+            //Ejercicio 1
+            System.out.println("***Tipos primitivos***");
+            System.out.println("byte: "+ Byte.BYTES + " byte");
+            System.out.println("short: "+Short.BYTES+ " bytes");
+            System.out.println("int: "+Integer.BYTES+ " bytes");
+            System.out.println("long: "+Long.BYTES+ " bytes");
+            System.out.println("float: "+Float.BYTES+ " bytes");
+            System.out.println("double: "+Double.BYTES+ " bytes");
+            System.out.println("boolean: 1 byte");
+            System.out.println("char: "+Character.BYTES+ " bytes");
+        }
+}
