5d4b83be4669f7f5441bb8a37ab6fd1f848ccebc
MeliLiz <melifernandez@ciencias.unam.mx>
2021-11-04 18:40:09 -0600

    Documentado

--- Cilindro.java
+++ Cilindro.java
@@ -1,14 +1,35 @@
-public class Cilindro {
-    public static void main (String[] args){
-        //Ejercicio 2
-        int radio = 10;
-        int altura = 20;
-        double volumen = 3.1416*altura*radio*radio;
-        double area = 2*3.1416*altura*radio + 2*3.1416*radio*radio;
+/**
+ * Clase Cilindro Clase para calcular el área y volumen del cilindro de radio 10
+ * cm y altura 20 cm.
+ * 
+ * @author Melissa Fernández
+ * @version 1.0
+ */
 
-       System.out.println("***Área y volumen de un cilindro***");
-       System.out.println("El área del cilindro es: "+ area + " cm²");
-       System.out.println("El volumen del cilindro es: "+ volumen + " cm³");
+public class Cilindro {
+    /**
+     * Método principal donde se calcula el volumen y área del cilindro
+     * 
+     * @param args Los argumentos
+     */
+    public static void main(String[] args) {
+        // Ejercicio 2
+
+        // El radio del cilindro
+        int radio = 10;
+        // La altura del cilindro
+        int altura = 20;
+
+        // Calcular el volumen del cilindro usando su altura y su radio.
+        double volumen = 3.1416 * altura * radio * radio;
+        // Calcular el área del cilindro usando su altura y radio.
+        double area = 2 * 3.1416 * altura * radio + 2 * 3.1416 * radio * radio;
+
+        System.out.println("***Área y volumen de un cilindro***");// Imprimir qué se está calculando
+        // Imprimir el área del cilindro
+        System.out.println("El área del cilindro es: " + area + " cm²");
+        // Imprimir el volumen del cilindro
+        System.out.println("El volumen del cilindro es: " + volumen + " cm³");
 
     }
 }
--- Primitivo.java
+++ Primitivo.java
@@ -1,14 +1,29 @@
+/**
+ * Clase Primitivo Clase para mostrar el número de bytes utilizados por los
+ * datos primitivos
+ * 
+ * @author Melissa Fernández
+ * @version 1.0
+ */
+
 public class Primitivo {
-        public static void main (String[] args){
-            //Ejercicio 1
-            System.out.println("***Tipos primitivos***");
-            System.out.println("byte: "+ Byte.BYTES + " byte");
-            System.out.println("short: "+Short.BYTES+ " bytes");
-            System.out.println("int: "+Integer.BYTES+ " bytes");
-            System.out.println("long: "+Long.BYTES+ " bytes");
-            System.out.println("float: "+Float.BYTES+ " bytes");
-            System.out.println("double: "+Double.BYTES+ " bytes");
-            System.out.println("boolean: 1 byte");
-            System.out.println("char: "+Character.BYTES+ " bytes");
-        }
+    /**
+     * Método principal donde se utiliza el método BYTES para imprimir el tamaño en
+     * bytes de los tipos primitivos.
+     * 
+     * @param args Los argumentos
+     */
+    public static void main(String[] args) {
+        // Ejercicio 1
+        System.out.println("***Tipos primitivos***"); // Indicar de qué trata el programa
+        // Imprimir el número de bytes de cada tipo primitivo
+        System.out.println("byte: " + Byte.BYTES + " byte");
+        System.out.println("short: " + Short.BYTES + " bytes");
+        System.out.println("int: " + Integer.BYTES + " bytes");
+        System.out.println("long: " + Long.BYTES + " bytes");
+        System.out.println("float: " + Float.BYTES + " bytes");
+        System.out.println("double: " + Double.BYTES + " bytes");
+        System.out.println("boolean: 1 byte");
+        System.out.println("char: " + Character.BYTES + " bytes");
+    }
 }
